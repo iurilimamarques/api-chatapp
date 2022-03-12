61bbfa439e5b150b2eca51b6db7dcbe3c3dc328d
Iuri de Lima Marques <iurilmarques@hotmail.com>
2022-03-11 23:45:15 -0300

    master - removing tests

--- src/test/java/com/chatappapi/api/ApiApplicationTests.java
+++ src/test/java/com/chatappapi/api/ApiApplicationTests.java
@@ -1,13 +1,8 @@
 package com.chatappapi.api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
